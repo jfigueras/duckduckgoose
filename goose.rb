68f26de247645d96c8a90a991cfa1af5e3677415
Jordi Figueras <jfigueras@searchinfluence.com>
2016-10-20 12:09:18 -0500

    with heritage impalmented

--- goose.rb
+++ goose.rb
@@ -1,31 +1,37 @@
 
 
-class Goose 
+class Goose < Player
 
-  attr_reader :name, :speed
-  attr_writer :speed
-  #attr_accessor :speed
+  attr_reader :name, :speed, :ayuke_arm
+  attr_accessor :speed 
 
-  def initialize(name: name )
-
-    @name = name
-    @speed = 4
-  
-  end
-
-  def call
-     
-  end
+  def post_initialize
+    @ayuke_arm = false
+  end  
 
   def introduction
     puts "player #{name} is the goose"
   end 
 
   def play
-    speed = rand()*10
+    self.speed = rand()*10
     puts "the goose runs at #{speed.round(2)}"
+    ayuke_power
+  end  
+  
+  def default_speed
+    4
   end  
 
   private
+
+  attr_writer :ayuke_arm
+
+  def ayuke_power
+    ayuke = rand()
+    if ayuke < 0.15
+      self.ayuke_arm = true
+    end 
+  end  
   
 end  
--- it.rb
+++ it.rb
@@ -1,29 +1,24 @@
 
-class It 
+class It < Player
 
   attr_reader :name, :goose
   attr_accessor :speed 
 
-  def initialize(name: name )
-
-    @name = name
-    @speed = 8
+  def post_initialize
     @goose = rand()
-    
-  end
-
-  def call
-    
-  end
+  end  
 
   def introduction
     puts "player #{name} is the IT"
-
   end  
 
   def play
-    @speed = rand()*10
-    puts "the it runs at #{@speed.round(2)}"
+    self.speed = rand()*10
+    puts "the it runs at #{speed.round(2)}"
+  end  
+
+  def default_speed
+    8
   end  
 
   
